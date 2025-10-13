d487e666d3b24b3ce2a657e12e38217eee9dfd71
sujitb2114 <oceancg122@gmail.com>
2025-10-13 20:58:43 +0900

    Update config.js

--- config.js
+++ config.js
@@ -1,4 +1,4 @@
-0x5bdc66cb7033Cf94587383F4d0fe93C72B0Fd044
+0xce288D396A38746771849b40F276B49971EE0eE6
 Hi frens, my telegram acc is blocked.
 I'll create a new one and write here.
 In the meanwhile, you can reach out to me via Teams app.
